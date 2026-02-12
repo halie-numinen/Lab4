3eb2f5544cf51fc73e97342ffa78b0404ac6a523
Kahlan Walcott <166679300+Kahlan-walcott@users.noreply.github.com>
2026-02-11 20:36:07 -0500

    adding malloc and finishing it.

--- multiThreadedServer.c
+++ multiThreadedServer.c
@@ -8,7 +8,7 @@
 #include <signal.h>
 
 void* doGreeting(void* arg); 
-void handleSigint(int sig);
+// void handleSigint(int sig);
 // global (shared and specific) data 
 volatile sig_atomic_t shutdown_requested = 0;
 
@@ -23,7 +23,6 @@ int main() {
     pthread_t thread1[500]; 
     // void *result1; 
     int threadStatus1; 
-    int joinStatus1;
 
     int numberOfFiles = 0; 
     char userFile[500];  // buffer
@@ -36,15 +35,16 @@ int main() {
         printf("Please input a file name: "); 
         if (fgets(userFile, 500, stdin) == NULL) {
             break;
-            
         }
-        numberOfFiles ++;
+        
         userFile[strcspn(userFile, "\n")] = '\0'; 
+        
         char *fileName = malloc(strlen(userFile) + 1);
         if (!fileName) {
             perror("malloc failed.");
             continue;
         } 
+        strcpy(fileName, userFile);
         // char userFile = ("Please input a file name: ");
         // scanf("%s", &userInput);
 
@@ -55,27 +55,25 @@ int main() {
         // char userFile = ("Please input a file name: ");
         // scanf("%s", &userInput);
     
-        threadStatus1 = pthread_create (&thread1[numberOfFiles], NULL,  doGreeting, &userFile[0]); 
+        threadStatus1 = pthread_create (&thread1[numberOfFiles], NULL,  doGreeting, fileName); 
         if (threadStatus1 != 0){ 
             fprintf (stderr, "Thread create error %d: %s\n", threadStatus1, strerror(threadStatus1)); 
-            exit (1); 
+            free(fileName);
+            continue;
         } 
+        numberOfFiles ++;
     }
-    // printf ("Parent sees %d\n", sharedData); 
-    // sharedData++; 
-    
-    // printf ("Parent sees %d\n", sharedData);
+
+    printf("\nProgram terminating\n");
+    printf("The total number of file request received: %d\n", numberOfFiles);
+    fflush(stdout);
     for(int i = 0; i < numberOfFiles; i++) {
-        joinStatus1 = pthread_join(thread1[i], NULL); 
-        if (joinStatus1 != 0) { 
-            fprintf (stderr, "Join error %d: %s\n", joinStatus1, strerror(joinStatus1)); 
-            exit (1); 
-        } 
+        pthread_join(thread1[i], NULL); 
     } 
     return 0; 
 } 
 void* doGreeting(void* myArgument) { 
-    char *myPtr[500] = {(char *)myArgument}; 
+    char *myPtr = (char *)myArgument; 
     // printf ("Child receiving %c initially sees %d\n", *myPtr, sharedData); 
     int probability = rand() % 100;
     if (probability < 80) {
@@ -85,7 +83,8 @@ void* doGreeting(void* myArgument) {
         int randomNumber = (rand() % 4) + 7;
         sleep(randomNumber);
     }
-    printf ("Child receiving %s now sees\n", *myPtr); 
+    printf ("The file accessed is: %s\n", myPtr); 
+    free(myPtr);
     return NULL; 
 }
 
